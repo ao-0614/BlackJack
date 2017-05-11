c11058be6f6d77765f50d6df462a7186186fbdd4
asct05 <asct05@T-PC05.asc-sys.local>
2017-05-11 15:25:58 +0900

    カードクラスの作成
    ブラックジャック実装のため

---
+++ ascSys/ascSys/Card.java
@@ -0,0 +1,19 @@
+package ascSys;
+
+public class Card {
+	private Suit suit;
+	private Num num;
+
+	public Card(Suit suit, Num num){
+		super();
+		this.suit = suit;
+		this.num = num;
+	}
+
+	public Suit getSuit(){
+		return suit;
+	}
+	public Num getNum(){
+		return num;
+	}
+}
